bf21a21d1ba11d8748d430197c484d87f2017a32
Ritikphenomenon <97295964+Ritikphenomenon@users.noreply.github.com>
2022-01-07 19:27:12 +0530

    Add files via upload

---
+++ first.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int phjgvyjhguyjgfgyf6urfrtt
+    6;
+    cout<<"enter any number of your choice:"<<endl;
+    cin>>phjgvyjhguyjgfgyf6urfrtt
+    ;
+    cout<<phjgvyjhguyjgfgyf6urfrtt
+    <<endl;
+    
+
+    
+    return 0;
